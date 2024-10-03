0e5226810da0612ce915b35f3d533f60bd971310
Arnav D <arnav.deepaware@gmail.com>
2024-10-02 22:33:51 -0400

    Added pygame information and started main function

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello World')
+import pygame
+import random
+
+#Dimensions for game window
+SCREEN_WIDTH = 960
+SCREEN_HEIGHT = 720
+
+#Colors used in the game: Black & White
+COLOR_BLACK = (0, 0, 0)
+COLOR_BLUE = (255, 255, 255)
+
+def main():
+
+    #Game Set up
+    pygame.init()
